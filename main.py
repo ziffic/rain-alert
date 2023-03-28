92bb6877a81fe75b6ddd7982a6126091c17241f0
David Yrigoyen <dyrigoyen@tyr.com>
2023-03-28 11:21:20 -0700

    fixed api key error

--- main.py
+++ main.py
@@ -4,16 +4,10 @@ import connect
 MY_LAT = 33.787914
 MY_LNG = -117.853104
 
-parameters = {
-    "api_key": connect.api_key,
-    "lat": MY_LAT,
-    "lon": MY_LNG
-}
-
 weather_response = requests.get(url=f"https://api.openweathermap.org/data/2.5/weather?"
                                     f"lat={MY_LAT}&"
                                     f"lon={MY_LNG}&"
-                                    f"appid={api_key}")
+                                    f"appid={connect.api_key}")
 weather_response.raise_for_status()
 weather_data = weather_response.json()
 
